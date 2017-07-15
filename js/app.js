8c5e6502e001f190c29b7ff0c238c0f95031d683
JohnnyKapps <jonatankapps@live.com>
2017-07-15 14:55:48 -0300

    First commit

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 //cria um modulo novo chamado agenda
 var app = angular.module('agenda', [])
+//
+// //cria um controller novo para esse modulo
+ app.controller('main', function($scope){
 
-//cria um controller novo para esse modulo
-app.controller('main', function($scope){
-  //cria uma variavel de escopo, no nosso caso, um array vazio
-  $scope.contacts = []
-  //cria um metodo de escopo
-  $scope.add = function(valid){
-    debugger;
-    //cria um objeto
-    if (valid) {
-      var contact = {
-        name: $scope.name,
-        last_name: $scope.last_name,
-        email: $scope.email
-      }
+//   //cria uma variavel de escopo, no nosso caso, um array vazio
+   $scope.contacts = []
+//   //cria um metodo de escopo
+   $scope.add = function(valid){
+     //cria um objeto
+     if (valid) {
+       var contact = {
+         name: $scope.name,
+         last_name: $scope.last_name,
+         email: $scope.email
+       }
 
-      console.log(contact)
-      //adiciona no array do escopo
-      $scope.contacts.push(contact)
-      $scope.name = ""
-      $scope.last_name = ""
-      $scope.email = ""
-    }
-    else{
-      $scope.error = "Formulario invalido, preencha os campos"
-    }
-  }
-})
+       //adiciona no array do escopo
+       $scope.contacts.push(contact)
+
+       $scope.name = ""
+       $scope.last_name = ""
+       $scope.email = ""
+     }
+     else{
+       $scope.error = "Formulario invalido, preencha os campos"
+     }
+   }
+ })
